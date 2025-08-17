fc6496a87f98c27a1582616172b3dc5125ba42ef
AhmedX2111 <Ahmedkhaled910000@gmail.com>
2025-08-17 22:03:18 +0300

    feat(cors): move frontend URL to appsettings.json
    - Added "Frontend:Url" config section in appsettings.json
    - Updated Program.cs to read frontend origin from configuration
    - Enabled centralized CORS policy management for easier environment setup

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -1,8 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Read frontend URL from appsettings
+var frontendUrl = builder.Configuration["Frontend:Url"];
+
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();               // Add Controllers
+builder.Services.AddEndpointsApiExplorer();      // Needed for Swagger
+
+// Register DbContext with Connection String from appsettings.json
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register your services here (DI)
+
+
+
+// Add CORS policy for frontend (Angular/React/...)
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowFrontend",
+		policy => policy
+			.WithOrigins(frontendUrl!)  // using value from appsettings.json
+			.AllowAnyHeader()
+			.AllowAnyMethod());
+});
 
 var app = builder.Build();
 
@@ -12,30 +38,21 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Enable CORS
+app.UseCors("AllowFrontend");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+
+// Map Controllers 
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+//var context = services.GetRequiredService<AppDbContext>();
+//context.Database.EnsureCreated();
+//context.Database.Migrate();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

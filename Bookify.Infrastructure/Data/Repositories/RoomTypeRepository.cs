4662dff3e668cd831a91b97fcd47e7009da9b396
Mohb Islam <mohabislam01@gmail.com>
2025-10-04 12:19:06 +0300

    Edit the Repositorys in Infrastructure [BookingRepository , RoomRepository , RoomTypeRepository]

--- Bookify.Infrastructure/Data/Repositories/RoomTypeRepository.cs
+++ Bookify.Infrastructure/Data/Repositories/RoomTypeRepository.cs
@@ -1,18 +1,27 @@
-﻿using Bookify.Application.Business.Interfaces.Data;
+﻿// Bookify-Hotel-Reservation-System-master/Bookify.Infrastructure/Data/Repositories/RoomTypeRepository.cs
+using Bookify.Application.Business.Interfaces.Data;
 using Bookify.Domain.Entities;
 using Bookify.Infrastructure.Data.Data.Context;
+using Microsoft.EntityFrameworkCore; // Added
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Bookify.Infrastructure.Data.Data.Repositories
 {
-	public class RoomTypeRepository : Repository<RoomType>, IRoomTypeRepository
-	{
-		public RoomTypeRepository(BookifyDbContext dbContext) : base(dbContext)
-		{
-		}
-	}
+    public class RoomTypeRepository : Repository<RoomType>, IRoomTypeRepository
+    {
+        public RoomTypeRepository(BookifyDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        // New: Get RoomType by ID
+        public async Task<RoomType?> GetRoomTypeByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.RoomTypes.FirstOrDefaultAsync(rt => rt.Id == id, cancellationToken);
+        }
+    }
 }
